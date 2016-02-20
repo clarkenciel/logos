e246559591abc855f15eb4f4d273d17699b63ca8
clarkenciel <danny@clarkenciel.com>
2016-02-20 12:01:49 -0800

    Customizing applet code for more control

--- src/logos/core.clj
+++ src/logos/core.clj
@@ -1,35 +1,26 @@
 (ns logos.core
-  (:use [logos.q])
-  (:require [quil.core :as q]
-            [quil.middleware :as m]))
+  (:use [logos.q]
+        [logos.applet]))
 
 ;; SETUP
 
 ;; need to create at least one quil applet before booting overtone
 ;; for some reason: https://github.com/overtone/overtone/issues/313
 ;; define and call this function simultaneously
-((defn safe-start []
-    (do
-      (q/defsketch foo :size [0 0])
-      (quil.applet/applet-disposed foo)
-      (require '(overtone [live :as ol]))
-      (use 'logos.sc))))
+(defn safe-start []
+  (do
+    (throwaway)
+    (require '(overtone [live :as ol]))
+    (use 'logos.sc)))
 
 ;; UTILS
 
 ;; TODO: should move fft-polling out of draw and into update
 
-(defn tap->rgb [ugen tap ulo uhi]
-  (q/map-range (get-tap-val ugen tap)
-               ulo uhi 0 255))
-
-
-
-(defn draw-text [state]
-  (q/background 0)
-  (q/fill 255)
-  (q/text (str 1) 250 250))
-
 ;; MAIN
-
-
+(defn setup [] nil)
+(defn updte [s] {:x 50 :y 50})
+(defn draw [{x :x y :y}]
+  (circle x y 10))
+(def vis (make-viz [200 200] setup updte draw))
+(run-viz vis "hi" :p3d)
--- src/logos/q.clj
+++ src/logos/q.clj
@@ -1,9 +1,10 @@
-(ns logos.q
+(ns logos.q ^{:doc "Collection of rendering functions for quil"}
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [clojure.string :as s]))
 
 ;; ==================== QUIL
 
+;; ===== GENERAL UTILS
 (defn fade-bg
   ([alpha]
    (fade-bg alpha (q/width) (q/height)))
@@ -11,6 +12,14 @@
    (q/fill 0 0 0 alpha)
    (q/rect 0 0 w h)))
 
+(defn val->rgb [val ulo uhi]
+  (q/map-range val ulo uhi 0 255))
+
+(defn circle [x y size]
+  (q/fill 0)
+  (q/ellipse x y size size))
+
+;; ===== HISTOGRAMS
 (defn draw-hist-rect
   ([x-start height width]
    (draw-hist-rect x-start (q/height) height width [125]))
@@ -37,11 +46,10 @@
     (doseq [[d i] (zipmap data (range l))]
       (q/text (str d) (+ x-start (* w i)) (- h d)))))
 
-(defn make-viz [setup update draw]
-  (q/sketch
-   :title ""
-   :size [500 500]
-   :setup setup
-   :update update
-   :draw draw
-   :middleware [m/fun-mode]))
+;; ===== TEXT BOXES
+
+(defn text-row [string])
+(defn text-box [words]
+  (q/background 0)
+  (q/fill 255)
+  (q/text words 250 250))
